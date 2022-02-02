2646d574e6e6e3850461ccfa5772391881141ad7
hagerondev <hagerondev@users.noreply.github.com>
2022-02-02 19:28:08 +0000

    deploy: f5684911d76765b168dceaa829d7393426e26fc4

--- _next/static/Muvp14NEOO0dFJ4Q1sl4F/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/36bcf0ca-a3d3cc3a28a5e90f.js","static/chunks/890-a4a0a7246ccb539f.js","static/chunks/pages/index-c904709da249e2ea.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/tBfGRNcXnI20C8Ckq2AOK/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/36bcf0ca-a3d3cc3a28a5e90f.js","static/chunks/890-a4a0a7246ccb539f.js","static/chunks/pages/index-606993845b5c83bd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
