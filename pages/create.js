6deee8c0b701e1bef08299369d9ea7aee653b4d6
hageron1229 <hageronprogram@gmail.com>
2022-02-06 16:27:49 +0900

    readme

--- pages/create.js
+++ pages/create.js
@@ -7,20 +7,15 @@ async function create(set,sset) {
         console.log("DDD")
         console.log(typeof window===undefined,typeof window!==undefined)
         const contest = document.getElementById("contest").value;
-        const friends = document.getElementById("friends").value;
+        const friends = document.getElementById("friends").value.replace(/\s+/g, "");
         const start = document.getElementById("start").value;
         const end = document.getElementById("end").value;
         const url = `https://hagerondev.github.io/atcoderfriends/?contest=${contest}&friends=${friends}&start=${start}&end=${end}`;
-        // const srurl = "http://is.gd/create.php?format=simple&format=json&url="+url
-        // console.log(srurl,encodeURI(srurl))
-        // const res = await fetch("http://tinyurl.com/api-create.php?url="+url);
-        // console.log(res)
-        // const d = await res.json()
+
+        
         set(url)
-        sset(url)
-        console.log(url)
-        // sset(d.shorturl)
-        // console.log(d)
+        
+        //sset(url)
     }
 }
 
@@ -30,10 +25,11 @@ export default function Home() {
     const [res, resset] = useState("");
 
     useEffect(() => {
+        //<div className="text-xl">短縮 : <Link href={screate_url}><a className="underline">{screate_url}</a></Link></div>
         resset(
             <div>
                 <div className="text-xl">URL : <Link href={create_url}><a className="underline">{create_url}</a></Link></div>
-                <div className="text-xl">短縮 : <Link href={screate_url}><a className="underline">{screate_url}</a></Link></div>
+                
             </div>
         )
     },[create_url])
@@ -45,7 +41,7 @@ export default function Home() {
                 <label>コンテスト名：<input type="text" id="contest" className="border-black border"/></label>
             </div>
             <div className="my-2">
-                <label>参加者：<input type="text" id="friends" className="border-black border"/></label>
+                <label>参加者：<input type="text" id="friends" className="border-black border" /></label>
                 <div>※カンマ(,)区切り</div>
             </div>
             <div className="my-2">
@@ -54,7 +50,9 @@ export default function Home() {
             <div className="my-2">
                 <label>終了UNIX TIME：<input type="text" id="end" className="border-black border"/></label>
             </div>
-            <button onClick={(e) => create(urlset, surlset)}>コンテストページ作成</button>
+            <br/>
+            <button className="border border-gray-600 rounded-md p-3 m-3" onClick={(e) => create(urlset)}>URL作成</button>
+            <br/>
             <div className="my-2">
                 {res}
             </div>
